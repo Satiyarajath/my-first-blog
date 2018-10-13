74424b8f80ff83e90539dbf91dbc2049a9df7c44
satiyarajath <38830700+Satiyarajath@users.noreply.github.com>
2018-10-13 08:55:11 +0530

    Corrected HTML template name

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def Post_List(request):
-    return render(request,'blog/post_list.html', {}) 
+    return render(request,'blog/Post_List.html', {}) 
