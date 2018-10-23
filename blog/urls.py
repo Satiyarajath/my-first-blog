83f6031d5b12edba944915acebd77bba7011438b
satiyarajath <38830700+Satiyarajath@users.noreply.github.com>
2018-10-23 16:55:38 +0530

    Added views for create/edit post for the site

--- blog/urls.py
+++ blog/urls.py
@@ -13,4 +13,6 @@ from . import views                         # . --> current directory
 urlpatterns = [
     path('',views.Post_List,name='Post_List'),   # named url - useful for reverse URL mapping
     path('post/<int:pk>/',views.Post_Detail,name='Post_Detail'),   # named url - for reverse lookup of each post
+    path('post/new/', views.Post_New, name='Post_New'), # named url - for reverse lookup - new post
+    path('post/<int:pk>/edit/',views.Post_Edit, name = 'Post_Edit') # reverse lookup - edit existig post
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render,get_object_or_404   # in case primary key,'pk' doesn't match
+from django.shortcuts import render, get_object_or_404, redirect   # in case primary key,'pk' doesn't match
 from .models import Post                # including models inside view to query the DB
                                         # and send value to template
                                         # . --> current directory
 
 from django.utils import timezone           # to use in query set
+
+from .forms import PostForm                 # to use PostForm in post_new
 
 
 # Create your views here.
@@ -19,3 +21,51 @@ def Post_Detail(request,pk):
     # get_object_or_404 --> to handle pk doesn't exist
     post = get_object_or_404(Post,pk=pk)
     return render(request,'blog/Post_Detail.html',{'post' : post})
+
+def Post_New(request):
+    # create a form object using PostForm and pass to template
+    # scenario 1 --> when form returns with value in "request.POST"
+    #                   a. create a form object with values from 'request.POST'
+    #                   b. if form is valid,
+    #                       1. update necessary fields in post
+    #                       2. save post
+    #                       3. redirect to post detail
+    # scenario 2 --> blank form
+
+    # scenario 1:
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("Post_Detail", pk=post.pk)
+    # scenario 2:
+    else:
+        form = PostForm()
+    return render(request,'blog/Post_Edit.html',{'form':form})
+
+def Post_Edit(request,pk):
+    # scenario 1 : edited post is returned in request.POST
+    #             a. create a form object using request.POST, instance = post
+    #             b. validate the form 
+    #             c. save post using values from form
+    #             d. update other fields in post
+    #             e. explicit save of post
+    #             f. redirect to detail page
+    # scenario 2 : (first time ) send existing post in edit html (instance = post)
+
+    post = get_object_or_404(Post,pk=pk)        # getting the post to be edited using pk
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("Post_Detail", pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request,'blog/Post_Edit.html',{'form':form})      # template reused
